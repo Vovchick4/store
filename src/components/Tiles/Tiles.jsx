03513ecdce384c76640eb57f8dda5b81bc1204d0
Vovchick4 <vovaspodpivas@gmail.com>
2022-03-30 15:36:12 +0300

    f

--- src/components/Tiles/Tiles.jsx
+++ src/components/Tiles/Tiles.jsx
@@ -1,4 +1,3 @@
-import React from "react";
 import styles from "./Tiles.module.css";
 import image from "../../images/Home.webp";
 import { NavLink } from "react-router-dom";
@@ -6,55 +5,71 @@ import { NavLink } from "react-router-dom";
 const path = "#";
 
 const Tiles = () => {
-    return (
-        <div className={styles.tiles}>
-            <div className={styles.tiles__container}>
-                <div className={styles.tiles__body}>
-                    <div className={styles.tiles__column}>
-                        <div className={styles.tiles__column__item}>
-                            <div className={styles.item__content}>
-                            <NavLink to={path} className={`${styles.item__image} ${styles._ibg}`}><img src={image}
-                                alt="Photo1" /></NavLink>
-                                    <h5 className={styles.item__title}>Tile 1 Heading</h5>
-                                <div className={styles.item__text}>Lorem ipsum dolor sit amet, consectetur adipisicing elit. 
-                                Consequatur asperiores ipsum quae cumque commodi rerum sunt voluptate. Quam.
-
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div className={styles.tiles__column}>
-                        <div className={styles.tiles__column__item}>
-                            <div className={styles.item__content}>
-                            <NavLink to={path} className={`${styles.item__image} ${styles._ibg}`}><img src={image}
-                                alt="Photo2" /></NavLink>
-                                    <h5 className={styles.item__title}>Tile 2 Heading</h5>
-                                <div className={styles.item__text}>Lorem ipsum dolor sit amet, consectetur adipisicing elit. 
-                                Consequatur asperiores ipsum quae cumque commodi rerum sunt voluptate. Quam.
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div className={styles.tiles__column}>
-                        <div className={styles.tiles__column__item}>
-                            <div className={styles.item__content}>
-                            <NavLink to={path} className={`${styles.item__image} ${styles._ibg}`} ><img src={image}
-                                alt="Photo3" />
-                            </NavLink>
-                                    <h5 className={styles.item__title}>Tile 3 Heading</h5>
-                                <div className={styles.item__text}>Lorem ipsum dolor sit amet, consectetur adipisicing elit. 
-                                Consequatur asperiores ipsum quae cumque commodi rerum sunt voluptate. Quam.
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div className={styles.tiles__btn}>
-                        <button>View more</button>
-                    </div>
+  return (
+    <div className={styles.tiles}>
+      <div className={styles.tiles__container}>
+        <div className={styles.tiles__body}>
+          <div className={styles.tiles__column}>
+            <div className={styles.tiles__column__item}>
+              <div className={styles.item__content}>
+                <NavLink
+                  to={path}
+                  className={`${styles.item__image} ${styles._ibg}`}
+                >
+                  <img src={image} alt="Photo1" />
+                </NavLink>
+                <h5 className={styles.item__title}>Tile 1 Heading</h5>
+                <div className={styles.item__text}>
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                  Consequatur asperiores ipsum quae cumque commodi rerum sunt
+                  voluptate. Quam.
                 </div>
+              </div>
             </div>
+          </div>
+          <div className={styles.tiles__column}>
+            <div className={styles.tiles__column__item}>
+              <div className={styles.item__content}>
+                <NavLink
+                  to={path}
+                  className={`${styles.item__image} ${styles._ibg}`}
+                >
+                  <img src={image} alt="Photo2" />
+                </NavLink>
+                <h5 className={styles.item__title}>Tile 2 Heading</h5>
+                <div className={styles.item__text}>
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                  Consequatur asperiores ipsum quae cumque commodi rerum sunt
+                  voluptate. Quam.
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className={styles.tiles__column}>
+            <div className={styles.tiles__column__item}>
+              <div className={styles.item__content}>
+                <NavLink
+                  to={path}
+                  className={`${styles.item__image} ${styles._ibg}`}
+                >
+                  <img src={image} alt="Photo3" />
+                </NavLink>
+                <h5 className={styles.item__title}>Tile 3 Heading</h5>
+                <div className={styles.item__text}>
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                  Consequatur asperiores ipsum quae cumque commodi rerum sunt
+                  voluptate. Quam.
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className={styles.tiles__btn}>
+            <button>View more</button>
+          </div>
         </div>
-    )
-}
+      </div>
+    </div>
+  );
+};
 
-export default Tiles
+export default Tiles;
