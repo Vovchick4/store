4115a792a95ca31de5af4ffef77e04dd0ffdd78c
Anatoliy Sarakhman <anatolej1111@gmail.com>
2022-03-30 12:51:36 +0300

    fixed

--- src/components/Tiles/Tiles.jsx
+++ src/components/Tiles/Tiles.jsx
@@ -1,6 +1,9 @@
 import React from "react";
 import styles from "./Tiles.module.css";
 import image from "../../images/Home.webp";
+import { NavLink } from "react-router-dom";
+
+const path = "#";
 
 const Tiles = () => {
     return (
@@ -10,8 +13,8 @@ const Tiles = () => {
                     <div className={styles.tiles__column}>
                         <div className={styles.tiles__column__item}>
                             <div className={styles.item__content}>
-                            <a className={`${styles.item__image} ${styles._ibg}`}><img src={image}
-                                alt="Photo1" /></a>
+                            <NavLink to={path} className={`${styles.item__image} ${styles._ibg}`}><img src={image}
+                                alt="Photo1" /></NavLink>
                                     <h5 className={styles.item__title}>Tile 1 Heading</h5>
                                 <div className={styles.item__text}>Lorem ipsum dolor sit amet, consectetur adipisicing elit. 
                                 Consequatur asperiores ipsum quae cumque commodi rerum sunt voluptate. Quam.
@@ -23,8 +26,8 @@ const Tiles = () => {
                     <div className={styles.tiles__column}>
                         <div className={styles.tiles__column__item}>
                             <div className={styles.item__content}>
-                            <a className={`${styles.item__image} ${styles._ibg}`}><img src={image}
-                                alt="Photo2" /></a>
+                            <NavLink to={path} className={`${styles.item__image} ${styles._ibg}`}><img src={image}
+                                alt="Photo2" /></NavLink>
                                     <h5 className={styles.item__title}>Tile 2 Heading</h5>
                                 <div className={styles.item__text}>Lorem ipsum dolor sit amet, consectetur adipisicing elit. 
                                 Consequatur asperiores ipsum quae cumque commodi rerum sunt voluptate. Quam.
@@ -35,9 +38,9 @@ const Tiles = () => {
                     <div className={styles.tiles__column}>
                         <div className={styles.tiles__column__item}>
                             <div className={styles.item__content}>
-                            <a className={`${styles.item__image} ${styles._ibg}`} ><img src={image}
+                            <NavLink to={path} className={`${styles.item__image} ${styles._ibg}`} ><img src={image}
                                 alt="Photo3" />
-                            </a>
+                            </NavLink>
                                     <h5 className={styles.item__title}>Tile 3 Heading</h5>
                                 <div className={styles.item__text}>Lorem ipsum dolor sit amet, consectetur adipisicing elit. 
                                 Consequatur asperiores ipsum quae cumque commodi rerum sunt voluptate. Quam.
