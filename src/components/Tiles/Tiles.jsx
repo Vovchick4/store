c2da4fa19d717e4d8a8067f7e952a0478576c6e0
Vovchick4 <vovaspodpivas@gmail.com>
2022-04-07 12:04:29 +0300

    add

--- src/components/Tiles/Tiles.jsx
+++ src/components/Tiles/Tiles.jsx
@@ -2,57 +2,24 @@ import React, { useState } from "react";
 import styles from "./Tiles.module.css";
 import { NavLink } from "react-router-dom";
 import image from "../../images/Home.webp";
-import data from "../../data/data.json";
 
 const path = "#";
 
-const Tiles = () => {
-  const [lenghtData, setLenghtData] = useState(3);
-
-  function moreData() {
-    if (lenghtData > data.length) return alert("MAX DATA");
-
-    setLenghtData((prev) => prev + 3);
-  }
-
-  function lessData() {
-    setLenghtData((prev) => prev - 3);
-  }
-
+const Tiles = (props) => {
   return (
-    <div className={styles.tiles}>
-      <div className={styles.tiles__container}>
-        <div className={styles.tiles__body}>
-          <div className={styles.tiles__column}>
-            {data &&
-              data.map(
-                (da) =>
-                  lenghtData >= da.id && (
-                    <div className={styles.item__content}>
-                      <NavLink
-                        to={path}
-                        className={`${styles.item__image} ${styles._ibg}`}
-                      >
-                        <img src={image} alt="Photo3" />
-                      </NavLink>
-                      <h5 className={styles.item__title}>Tile 3 Heading</h5>
-                      <div className={styles.item__text}>
-                        Lorem ipsum dolor sit amet, consectetur adipisicing
-                        elit. Consequatur asperiores ipsum quae cumque commodi
-                        rerum sunt voluptate. Quam.
-                      </div>
-                    </div>
-                  )
-              )}
+    <div className={styles.tiles__column}>
+      <div className={styles.tiles__column__item}>
+        <div className={styles.item__content}>
+          <a href={path} className={`${styles.item__image} ${styles._ibg}`}>
+            <img src={props.data.image} alt="Photo1" />
+          </a>
+          <h5 className={styles.item__title}>Tile {props.data.id} Heading</h5>
+          <div className={styles.item__text}>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+            Consequatur asperiores ipsum quae cumque commodi rerum sunt
+            voluptate. Quam.
           </div>
         </div>
-      </div>
-
-      <div className={styles.tiles__btn}>
-        {data.length > lenghtData && (
-          <button onClick={moreData}>View more</button>
-        )}
-        {lenghtData !== 3 && <button onClick={lessData}>Collopase data</button>}
       </div>
     </div>
   );
--- src/components/Tiles/TilesContainer.jsx
+++ src/components/Tiles/TilesContainer.jsx
@@ -6,35 +6,42 @@ import { TransitionGroup, CSSTransition } from "react-transition-group";
 import fadeIn from "../../css/anim/fadeIn.module.css";
 
 const TilesContainer = () => {
-    const [visible, setVisible] = useState(3);
+  const [visible, setVisible] = useState(3);
 
-    const showMoreItems = () => {
-        setVisible(visible + 3);
-    }
+  const showMoreItems = () => {
+    setVisible((prev) => prev + 3);
+  };
 
-    return (
-        <div className={styles.tiles}>
-            <div className={styles.tiles__container}>
-                <TransitionGroup className={styles.tiles__body}>
-                    {tilesData.slice(0, visible).map((e) => (
-                        <CSSTransition
-                        key={e.id}
-                        classNames={fadeIn}
-                        timeout={500}
-                        unmountOnExit
-                      >
-                        <Tiles tilesData={tilesData} data={e} />
-                      </CSSTransition>
+  const collapseItems = () => {
+    setVisible((prev) => prev - 3);
+  };
 
-                    ))}
-                    </TransitionGroup>
-            </div>
-            <div className={styles.tiles__btn}>
-                <button onClick={showMoreItems}>View more</button>
-            </div>
-        </div>
-    );
+  return (
+    <div className={styles.tiles}>
+      <div className={styles.tiles__container}>
+        <TransitionGroup className={styles.tiles__body}>
+          {tilesData.slice(0, visible).map((e) => (
+            <CSSTransition
+              key={e.id}
+              classNames={fadeIn}
+              timeout={500}
+              unmountOnExit
+            >
+              <Tiles tilesData={tilesData} data={e} />
+            </CSSTransition>
+          ))}
+        </TransitionGroup>
+      </div>
+      <div className={styles.tiles__btn}>
+        {tilesData.length > visible && (
+          <button onClick={showMoreItems}>View more</button>
+        )}
+        {visible !== 3 && (
+          <button onClick={collapseItems}>Collapse Data</button>
+        )}
+      </div>
+    </div>
+  );
 };
 
 export default TilesContainer;
-
