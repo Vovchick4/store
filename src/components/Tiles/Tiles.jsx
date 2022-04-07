1e3dcbd435f0987c7c2c6cc3368dedf98f9a4c90
Vovchick4 <vovaspodpivas@gmail.com>
2022-04-07 11:43:07 +0300

    add upload more data

--- src/components/Tiles/Tiles.jsx
+++ src/components/Tiles/Tiles.jsx
@@ -1,72 +1,58 @@
 import styles from "./Tiles.module.css";
 import image from "../../images/Home.webp";
 import { NavLink } from "react-router-dom";
+import data from "../../data/data.json";
+import { useState } from "react";
 
 const path = "#";
 
 const Tiles = () => {
+  const [lenghtData, setLenghtData] = useState(3);
+
+  function moreData() {
+    if (lenghtData > data.length) return alert("MAX DATA");
+
+    setLenghtData((prev) => prev + 3);
+  }
+
+  function lessData() {
+    setLenghtData((prev) => prev - 3);
+  }
+
   return (
     <div className={styles.tiles}>
       <div className={styles.tiles__container}>
         <div className={styles.tiles__body}>
           <div className={styles.tiles__column}>
-            <div className={styles.tiles__column__item}>
-              <div className={styles.item__content}>
-                <NavLink
-                  to={path}
-                  className={`${styles.item__image} ${styles._ibg}`}
-                >
-                  <img src={image} alt="Photo1" />
-                </NavLink>
-                <h5 className={styles.item__title}>Tile 1 Heading</h5>
-                <div className={styles.item__text}>
-                  Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-                  Consequatur asperiores ipsum quae cumque commodi rerum sunt
-                  voluptate. Quam.
-                </div>
-              </div>
-            </div>
-          </div>
-          <div className={styles.tiles__column}>
-            <div className={styles.tiles__column__item}>
-              <div className={styles.item__content}>
-                <NavLink
-                  to={path}
-                  className={`${styles.item__image} ${styles._ibg}`}
-                >
-                  <img src={image} alt="Photo2" />
-                </NavLink>
-                <h5 className={styles.item__title}>Tile 2 Heading</h5>
-                <div className={styles.item__text}>
-                  Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-                  Consequatur asperiores ipsum quae cumque commodi rerum sunt
-                  voluptate. Quam.
-                </div>
-              </div>
-            </div>
-          </div>
-          <div className={styles.tiles__column}>
-            <div className={styles.tiles__column__item}>
-              <div className={styles.item__content}>
-                <NavLink
-                  to={path}
-                  className={`${styles.item__image} ${styles._ibg}`}
-                >
-                  <img src={image} alt="Photo3" />
-                </NavLink>
-                <h5 className={styles.item__title}>Tile 3 Heading</h5>
-                <div className={styles.item__text}>
-                  Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-                  Consequatur asperiores ipsum quae cumque commodi rerum sunt
-                  voluptate. Quam.
-                </div>
-              </div>
-            </div>
-          </div>
-          <div className={styles.tiles__btn}>
-            <button>View more</button>
+            {data &&
+              data.map(
+                (da) =>
+                  lenghtData >= da.id && (
+                    <div className={styles.item__content}>
+                      <NavLink
+                        to={path}
+                        className={`${styles.item__image} ${styles._ibg}`}
+                      >
+                        <img src={image} alt="Photo3" />
+                      </NavLink>
+                      <h5 className={styles.item__title}>Tile 3 Heading</h5>
+                      <div className={styles.item__text}>
+                        Lorem ipsum dolor sit amet, consectetur adipisicing
+                        elit. Consequatur asperiores ipsum quae cumque commodi
+                        rerum sunt voluptate. Quam.
+                      </div>
+                    </div>
+                  )
+              )}
           </div>
         </div>
+      </div>
+
+      <div className={styles.tiles__btn}>
+        {data.length > lenghtData && (
+          <button onClick={moreData}>View more</button>
+        )}
+        {lenghtData !== 3 && <button onClick={lessData}>Collopase data</button>}
       </div>
     </div>
   );
